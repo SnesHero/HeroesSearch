9cb6849b23785fba6c9a7b7e7b56b001474755c9
jojotelhada <carlos.l.telhada@gmail.com>
2021-12-07 10:55:20 -0300

    Account change test

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -7,6 +7,7 @@ export const HomePage = () => {
     return(
         <div className="home">
             <h1 className="title">Super Nelson Wars</h1>
+            <div>Account change test</div>
            <Searchbar/>
 
             <div >
